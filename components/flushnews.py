15ad1e839d7a638b16d71a531f49d350881be683
Paul M. Watson <github.com@paulmwatson.com>
2017-09-28 14:17:05 +0200

    Reset content refresh time stamp
    When resetting content also reset the time stamp for refresh otherwise the user has to wait till after the time stamp to get any news.

--- components/flushnews.py
+++ components/flushnews.py
@@ -5,6 +5,8 @@ from meya import Component
 class HelloWorld(Component):
 
     def start(self):
+        # reset content refresh time stamp
+        self.db.bot.set('content_refresh_ts', 0)
         # flush the content database
         content_list = self.db.table('content').filter()
         for content in content_list:
